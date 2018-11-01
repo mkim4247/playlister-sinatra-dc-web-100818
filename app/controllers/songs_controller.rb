d9ef5086052dff99ea0779f5e921f594a25ed4b5
github username <github email address>
2018-11-01 18:05:14 -0400

    order the routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,17 +5,20 @@ class SongsController < ApplicationController
     @songs = Song.all
     erb :"songs/index"
   end
-
-  get '/songs/:slug' do
-  	binding.pry
-    @song = Song.find_by_slug(params[:slug])
-    erb :"songs/show"
-  end
-
+  
   get '/songs/new' do
   	erb :"songs/new"
 
   end
+
+  get '/songs/:slug' do
+
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :"songs/show"
+  end
+
+
 
   post '/songs' do
 
