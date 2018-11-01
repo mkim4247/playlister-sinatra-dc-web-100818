530c95a62002180e0f3b4847ca70e5ba57c1ba78
github username <github email address>
2018-11-01 18:20:14 -0400

    checkboxs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,20 +7,18 @@ class SongsController < ApplicationController
   end
   
   get '/songs/new' do
+  	 @artists = Artist.all 
   	erb :"songs/new"
 
   end
 
   get '/songs/:slug' do
-
     @song = Song.find_by_slug(params[:slug])
-
     erb :"songs/show"
   end
 
-
-
   post '/songs' do
+  	song = Song.create(name: params["song"]["name"], artist: params["song"]["artist"], genre: params["song"]["genre"])
 
   end
 
