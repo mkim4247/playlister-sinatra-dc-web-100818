f3b2097ff419385da364e86982b1ab3d6f13a7a5
github username <github email address>
2018-11-01 17:54:02 -0400

    nick's commits

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,8 +7,18 @@ class SongsController < ApplicationController
   end
 
   get '/songs/:slug' do
+  	binding.pry
     @song = Song.find_by_slug(params[:slug])
     erb :"songs/show"
+  end
+
+  get '/songs/new' do
+  	erb :"songs/new"
+
+  end
+
+  post '/songs' do
+
   end
 
 end
